ae43870410a56543e79bdbce00c2b229f7c3652b
Jakub Pitera <pitjakub@gmail.com>
2021-01-04 16:29:46 +0100

    Added i18n support. Added support i18n to emails.

---
+++ src/main/java/pl/chatme/config/LocaleConfiguration.java
@@ -0,0 +1,49 @@
+package pl.chatme.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfiguration implements WebMvcConfigurer {
+
+    // Registration interceptor
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(localeChangeInterceptor());
+    }
+
+    // Changing locale by param ?lang=en
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    // Configuring cookie lang
+    @Bean
+    public LocaleContextResolver localeResolver() {
+        var cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return cookieLocaleResolver;
+    }
+
+    // Configuring ResourceBundle
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        var messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+
+}
---
+++ src/main/java/pl/chatme/util/Translator.java
@@ -0,0 +1,28 @@
+package pl.chatme.util;
+
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class Translator {
+
+    private final ResourceBundleMessageSource resourceBundleMessageSource;
+
+    public Translator(ResourceBundleMessageSource resourceBundleMessageSource) {
+        this.resourceBundleMessageSource = resourceBundleMessageSource;
+    }
+
+    public String translate(String code) {
+        return resourceBundleMessageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
+//    public String translateWithLocale(String code, Locale locale) {
+//        if (locale != null)
+//            return resourceBundleMessageSource.getMessage(code, null, locale);
+//        else return translateWithLocaleContextHolder(code);
+//    }
+
+}
