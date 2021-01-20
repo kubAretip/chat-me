cde5b5b7039dcf606b0df52a531847f292dd9ba1
Jakub Pitera <pitjakub@gmail.com>
2021-01-20 10:55:19 +0100

    Deleted unique index for password_hash column in user table.

--- src/main/resources/db/migration/V1_0_0__create_table_user.sql
+++ src/main/resources/db/migration/V1_0_0__create_table_user.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS user
 (
     id            BIGINT              NOT NULL auto_increment,
     login         varchar(50)         not null unique,
-    password_hash varchar(60)         not null unique,
+    password_hash varchar(60)         not null,
     first_name    varchar(50),
     last_name     varchar(50),
     email         varchar(254) unique not null,
